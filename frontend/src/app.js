ac79670d54fcd21396450dc4750bcb083d16607a
Ayush Kumar <ayush1031kumar@gmail.com>
2025-08-27 13:45:10 +0530

    Some frontend changes

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,10 +1,19 @@
 import React from 'react';
+// import {BrowserRouter as Router, Route , Routes} from 'react-router-dom';
+// import DragDrop from "./components/DragDrop";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, React!</h1>
-    </div>
+    <>
+      <div className='text-3xl text-red-700'>
+        <h1>Hello, React!</h1>
+      </div>
+      {/* <Router>
+        <Routes>
+          <Route path="/dragndrop" element={<DragDrop />} />
+        </Routes>
+      </Router> */}
+    </>
   );
 }
 

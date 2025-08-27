3cf9f6485449472773dd1772a0e2ad54e0bc02e9
Ayush Kumar <ayush1031kumar@gmail.com>
2025-08-27 13:45:10 +0530

    Some frontend changes

---
+++ frontend/src/components/DragDrop.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const DragDrop = () => {
+  return (
+    <div>
+      {/* this is a drag-drop page where the content will be made to be added. */}
+      <div>
+       <p> Drag and drop your question here</p>
+      </div>
+    </div>
+  )
+}
+
+export default DragDrop

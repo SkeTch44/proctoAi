38b1691679c367752d764d41dfcb863541498508
Ayush Kumar <ayush1031kumar@gmail.com>
2025-08-29 23:32:01 +0530

    Theme button and theme functionality is setup with all the updates of environment, all error are clared.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-  <App/>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+reportWebVitals();
